a0cb3ff0aaa3d95615f710cb8854b342085601bb
luciousdev <lucy.puyenbroek@gmail.com>
2023-10-10 20:01:24 +0200

    routes changed

--- resources/views/desktop.blade.php
+++ resources/views/desktop.blade.php
@@ -1,22 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>My Page</title>
-</head>
-<body>
-    <h1>Welcome to my page</h1>
-    <p>This is some sample text.</p>
-</body>
-</html>
-<!DOCTYPE html>
-<html>
-<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lucy Puyenbroek - Desktop</title>
-    <link rel="stylesheet" href="path/to/your/css/file.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
-    <!-- Your HTML content goes here -->
+    test
 </body>
 </html>
