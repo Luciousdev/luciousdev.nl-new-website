3e7f3304ec50f160b64c7fc999db1464bfedaf1c
luciousdev <lucy.puyenbroek@gmail.com>
2023-10-10 19:58:48 +0200

    first commit

---
+++ resources/views/desktop.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Page</title>
+</head>
+<body>
+    <h1>Welcome to my page</h1>
+    <p>This is some sample text.</p>
+</body>
+</html>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lucy Puyenbroek - Desktop</title>
+    <link rel="stylesheet" href="path/to/your/css/file.css">
+</head>
+<body>
+    <!-- Your HTML content goes here -->
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="{{ asset('css/boot.css') }}">
+</head>
+<body>
+    <pre id="boot-sequence"></pre>
+    <span class="blinking-cursor">|</span>
+
+    <script src="{{ asset('js/boot.js') }}"></script>
+</body>
+</html>
